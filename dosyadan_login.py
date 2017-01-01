2835e1adcdf01295db6b662ad95634b48579d086
efe <honoriscausadei1@gmail.com>
2017-01-01 02:48:04 +0200

    Create dosyadan_login.py
    Dosyadan admin bilgilerini alarak login

---
+++ dosyadan_login.py
@@ -0,0 +1,13 @@
+kadi = input("Kullanıcı Adı Girin: ")
+sifre = input("Sifre Girin: ")
+
+dosya = open("deneme.txt")
+a = dosya.read()
+
+if kadi in a and sifre in a:
+    if kadi == "admin" and sifre == "sifre":
+        print("Giriş Başarılı")
+    else:
+        print("Kullanıcı adı veya Paralo hatalı!")
+else:
+    print("Kullanıcı adı ve paralo veritabanına kayıtlı değil!")
